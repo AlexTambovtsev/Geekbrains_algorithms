a217e56f29cbb0275806fb8596374f9a07b3acd4
Alexey Tambovtsev <tambovtsev@list.ru>
2018-03-23 10:09:43 +0300

    fixed avtomorf

--- src/ru/geekbrains/algoritms/lesson_1/AvtomorfNumbers.java
+++ src/ru/geekbrains/algoritms/lesson_1/AvtomorfNumbers.java
@@ -3,6 +3,7 @@ package ru.geekbrains.algoritms.lesson_1;
 import java.util.Scanner;
 
 public class AvtomorfNumbers {
+    // Проверяю только числа, заканчивающиеся на 5, или 6.
     public void numOfNumbers() {
         Scanner input = new Scanner(System.in);
         System.out.println("Input the natural number:");
@@ -12,11 +13,12 @@ public class AvtomorfNumbers {
         }
         else {
             int counter=2;
-            for (int i = 5; i < number+1; i+=10) {
-                for (int j=i; j<i+2; j++) {
-                    if (avtomorf(j)) {
-                        counter++;
-                    }
+            for (int i = 6; i < number+1; i+=10) {
+                if (avtomorf(i)) {
+                    counter++;
+                }
+                if (avtomorf(i-1)) {
+                    counter++;
                 }
             }
             System.out.printf("Number of avtomorf numbers is: %d\n", counter);
